d1a4eaca7025b6ec7df7378d2b8b7082a4c77818
Patrick Sliwinski <patrick.sliwinski@gatech.edu>
2023-03-01 23:23:23 -0500

    Add line chart for cumulative project time

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,4 +8,15 @@ class Project < ApplicationRecord
       data: self.sessions.group_by_day(:date).sum(:time).to_a
     }
   end
+
+  def for_line_chart
+    data = Hash.new
+    data[self.created_at.to_datetime] = 0
+    cumulative_time = 0
+    self.sessions.order(date: :asc).each do |session|
+      cumulative_time += session.time
+      data[session.date.to_datetime] = cumulative_time / 3600.0
+    end
+    data
+  end
 end
