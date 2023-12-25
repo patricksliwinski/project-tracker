aeb9dd679d0b05077cc7cbd2e0576ed8768d1575
Patrick <patrick.sliwinski@outlook.com>
2023-12-25 00:13:22 -0500

    Add cumulative time to timeline

--- app/assets/javascript/timeline.js
+++ app/assets/javascript/timeline.js
@@ -6,6 +6,8 @@ var timelineData = parseTimeline();
 var timelineCtx = timelineCanvas.getContext('2d');
 window.addEventListener('resize', resizeTimeline, false);
 
+const eventGap = 120;
+
 timelineCanvas.addEventListener('click', function(e) {
     let canvasUrl = timelineCanvas.toDataURL();
     const tempLink = document.createElement('a');
@@ -23,25 +25,28 @@ function drawTimeline() {
 
     
     for (var i = 0; i < timelineData.length; i++) {
-        var centerY = 50 + i * 100;
+        var centerY = 50 + i * eventGap;
         timelineCtx.beginPath();
         timelineCtx.moveTo(timelineCanvas.width / 2, centerY);
         timelineCtx.arc(timelineCanvas.width / 2, centerY, 20, 0, 2 * Math.PI);
         timelineCtx.moveTo(timelineCanvas.width / 2, centerY);
-        timelineCtx.lineTo(timelineCanvas.width / 2, centerY + 100);
+        timelineCtx.lineTo(timelineCanvas.width / 2, centerY + eventGap);
         timelineCtx.fill();
         timelineCtx.stroke();
         timelineCtx.font = "bold 20px serif";
         timelineCtx.fillText(timelineData[i][0], timelineCanvas.width / 2 + 30, centerY + 5);
         timelineCtx.font = "20px serif";
         timelineCtx.fillText(timelineData[i][1].substring(0, 10), timelineCanvas.width / 2 + 30, centerY + 30);
+        if (timelineData[i][2]) {
+            timelineCtx.fillText(timelineData[i][2], timelineCanvas.width / 2 + 30, centerY + 55);
+        }
     }
 
 }
 
 function resizeTimeline() {
     timelineCanvas.width = timelineParent.offsetWidth - 24;
-    timelineCanvas.height = timelineData.length * 100 + 200;
+    timelineCanvas.height = timelineData.length * 120 + 200;
     drawTimeline();
 }
 
